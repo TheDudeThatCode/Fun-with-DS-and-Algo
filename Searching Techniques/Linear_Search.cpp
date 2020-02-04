516548ae9d4df5a87411f126bb5bfe13c14f45e1
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-02-04 13:10:26 +0530

    Create Linear_Search.cpp

---
+++ Searching Techniques/Linear_Search.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int Linear_Search(int key, int size, int arr[]) // Function for applying Linear Search where key is the element to be searched.
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        if (key == arr[i]) // If they are equal therefore the given key value is found in the array.
+        {
+            return i; // The function will return the index value of the searched element present in the array.
+            break;
+        }
+        
+    }
+    
+    return -1; // If the entered key element is not found.
+
+} // End Linear_Search function
+
+int main() // Main Section
+{
+
+    int key_val, arr_size;
+
+    cout << "Enter the size of the array : ";
+    cin >> arr_size; // Enter size of the array.
+    int array[arr_size], i;
+
+    cout << "\nEnter the array elements : ";
+    for (i = 0; i < arr_size; i++)
+    {
+        cin >> array[i]; // Inserting elements into the array.
+    }
+
+    cout << "\nEnter the key value to be searched : ";
+    cin >> key_val;                                       // Insert the key value to be searched in  the array.
+
+    int result = Linear_Search(key_val, arr_size, array); // Calling Linear_Search function and assigning its value to result.
+
+    if (result == -1)
+    {
+        cout << "The given key value is not found !!";
+    }
+
+    else
+    {
+        cout << "The given key value is found at the index value : " << result;
+    }
+    
+    return 0;
+}
