7ec58fc219f2e71f67aa5391177dc10524e66de2
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-20 14:05:00 +0530

    Update D1_DictionaryMethods.py

--- 100 Days of Code/D1_DictionaryMethods.py
+++ 100 Days of Code/D1_DictionaryMethods.py
@@ -1,3 +1,6 @@
+#Day 1 - Dictionary Methods
+
+
 user = {
     'basket' : [1,2,3],
     'greet' : 'hello',
@@ -9,3 +12,17 @@ print('home' in user.values())
 print('hello' in user.values())
 user.update({'age' : 30 })
 print(user)
+
+
+"""
+
+OUTPUT
+
+
+20
+True
+False
+True
+{'basket': [1, 2, 3], 'greet': 'hello', 'age': 30}
+
+"""
