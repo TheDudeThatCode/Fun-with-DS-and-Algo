cdd38871bf2389a7879ec6bd251021e0fac4f15c
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-24 01:03:31 +0530

    Update Codechef_FLOW006.cpp

--- 30 Days of Code/Day 2/Codechef_FLOW006.cpp
+++ 30 Days of Code/Day 2/Codechef_FLOW006.cpp
@@ -28,26 +28,30 @@ int main()
 
 /*
 
-Problem Statement 
+Problem Statement :-
 You're given an integer N. Write a program to calculate the sum of all the digits of N.
 
-Input
+Input :-
 The first line contains an integer T, total number of testcases. Then follow T lines, each line contains an integer N.
 
-Output
+Output :-
 Calculate the sum of digits of N.
 
-Constraints
+Constraints :-
 1 ≤ T ≤ 1000
 1 ≤ N ≤ 1000000
-Example
-Input
+
+Example :-
+
+Input :-
 3 
 12345
 31203
 2123
-Output
+
+Output :-
 15
 9
 8
+
 */
