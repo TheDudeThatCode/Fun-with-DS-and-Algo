7fb4f734fa3de9f550afb52093596de9d7c8d901
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-24 01:02:19 +0530

    Update Codechef_FLOW006.cpp

--- 30 Days of Code/Day 2/Codechef_FLOW006.cpp
+++ 30 Days of Code/Day 2/Codechef_FLOW006.cpp
@@ -12,3 +12,42 @@ int digit_sum( int n )  // Function for sum of digits of a number
       }
     return sum;  
 }
+int main()
+{
+    int t, num;
+    cin>>t;
+    while(t--)
+    {
+       cin>>num;
+       cout<<digit_sum(num)<<endl;
+    }
+    return 0;
+}
+
+
+
+/*
+
+Problem Statement 
+You're given an integer N. Write a program to calculate the sum of all the digits of N.
+
+Input
+The first line contains an integer T, total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output
+Calculate the sum of digits of N.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ N ≤ 1000000
+Example
+Input
+3 
+12345
+31203
+2123
+Output
+15
+9
+8
+*/
