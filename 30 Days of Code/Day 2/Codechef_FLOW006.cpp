e85f11c22cecd6de4b7a333a1cc1fbbca0de104f
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-24 01:00:46 +0530

    Update Codechef_FLOW006.cpp

--- 30 Days of Code/Day 2/Codechef_FLOW006.cpp
+++ 30 Days of Code/Day 2/Codechef_FLOW006.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
 using namespace std;
+
+int digit_sum( int n )  // Function for sum of digits of a number
+{
+    int r, sum = 0;
+    while(n != 0)
+      {   
+          r = n % 10;
+          sum = sum + r;
+          n = n / 10;
+      }
+    return sum;  
+}
