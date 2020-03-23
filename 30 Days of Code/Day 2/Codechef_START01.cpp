dbd294a8c99c5345248d8ab04eaeab5b2c2a077f
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-23 23:07:33 +0530

    Update Codechef_START01.cpp

--- 30 Days of Code/Day 2/Codechef_START01.cpp
+++ 30 Days of Code/Day 2/Codechef_START01.cpp
@@ -8,3 +8,25 @@ int main()
 	cout<<n;
 	return 0;
 }
+
+
+/*
+
+Problem Statement
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 ≤ n ≤ 105
+Sample Input
+123
+Sample Output
+123
+
+
+*/
