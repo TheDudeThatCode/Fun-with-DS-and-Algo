52f35bc849956556b50d65df66fb2091eee78662
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-25 00:07:12 +0530

    Update Codechef_FLOW002.cpp

--- 30 Days of Code/Day 3/Codechef_FLOW002.cpp
+++ 30 Days of Code/Day 3/Codechef_FLOW002.cpp
@@ -19,3 +19,31 @@ int main()
     }
     return 0;
 }
+
+
+/*
+
+Write a program to find the remainder when two given numbers are divided.
+
+Input
+The first line contains an integer T, total number of test cases. Then follow T lines, each line contains two Integers A and B.
+
+Output
+Find remainder when A is divided by B.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ A,B ≤ 10000
+Example
+Input
+3 
+1 2
+100 200
+10 40
+
+Output
+1
+100
+10
+
+*/
