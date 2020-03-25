66ece2fd9e4f60949c0a4d4a60ee5caad60ecf92
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-25 20:50:05 +0530

    Update Tuts_Sets_n_SetMethods.py

--- 30 Days of Code/Day 3/Tuts_Sets_n_SetMethods.py
+++ 30 Days of Code/Day 3/Tuts_Sets_n_SetMethods.py
@@ -1,1 +1,38 @@
+my_set = {1,2,3,4,5}
+your_set = {4,5,6,7,8,9,10}
+
+# PLEASE NOTE FOR EACH METHOD REMOVE THE # SIGN
+
+# 1 - .copy()
+# new_set = my_set.copy()
+# print(new_set)
+
+# 2 - .clear()
+# my_set.clear()
+
+# 3 - .difference()
+# print(my_set.difference(your_set))
+
+# 4 - .discard()
+# my_set.discard(5)
+# print(my_set)
+
+# 5 - .difference_update()
+# my_set.difference_update(your_set)
+# print(my_set)
+
+# 6 - .intersection()
+# print(my_set.intersection(your_set))
+
+# 7 - .isdisjoint()
+# print(my_set.isdisjoint(your_set))
+
+# 8 - .union()
+# print(my_set.union(your_set))
+
+# 9 - .issubset()
+# print(my_set.issubset(your_set))
+
+# 10 - .issuperset()
+# print(my_set.issuperset(your_set))
 
