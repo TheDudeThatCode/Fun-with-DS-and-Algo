8ed2e98bf7c04be47347ea8a18a99fb5dcc995bd
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-26 22:43:53 +0530

    Update Tut_Insertion_Sort.cpp

--- 30 Days of Code/Day 4/Tut_Insertion_Sort.cpp
+++ 30 Days of Code/Day 4/Tut_Insertion_Sort.cpp
@@ -20,3 +20,12 @@ void Insertion_Sort(int size, int arr[]) // Function for applying Insertion Sort
     }
 
 } // End Insertion_Sort function
+
+int main() // Main Section
+{
+
+    int arr_size;
+
+    cout << "Enter the size of the array : ";
+    cin >> arr_size; // Enter size of the array.
+    int array[arr_size], i;
