71e3d6430b6911d15f7b64586d614fcec5bee8df
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-27 02:27:23 +0530

    added main function.

--- 30 Days of Code/Day 5/Tut_Selection_Sort.cpp
+++ 30 Days of Code/Day 5/Tut_Selection_Sort.cpp
@@ -26,3 +26,37 @@ void Selection_Sort(int size, int arr[]) // Function for applying Selection Sort
     }
 
 } // End Selection_Sort function
+
+
+
+int main() // Main Section
+{
+
+    int arr_size;
+
+    cout << "Enter the size of the array : ";
+    cin >> arr_size; // Enter size of the array.
+    int array[arr_size], i;
+
+    cout << "\nEnter the array elements : ";
+    for (i = 0; i < arr_size; i++)
+    {
+        cin >> array[i]; // Inserting elements into the array.
+    }
+
+    cout << "\nThe array before applying Selection Sort Algorithm : ";
+    for (i = 0; i < arr_size; i++)
+    {
+        cout << " " << array[i]; // Printing elements of the array.
+    }
+
+    Selection_Sort(arr_size, array); // Selection_Sort function.
+
+    cout << "\n\nThe array after applying Selection Sort Algorithm : ";
+    for (i = 0; i < arr_size; i++)
+    {
+        cout << " " << array[i]; // Printing elements of the array.
+    }
+
+    return 0;
+} // End of program
