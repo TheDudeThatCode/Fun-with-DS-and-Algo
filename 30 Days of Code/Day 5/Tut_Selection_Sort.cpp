53255d00856dbe1c054226ab99980cbb308fe475
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-27 02:26:46 +0530

    Added Selection Sort Function.

--- 30 Days of Code/Day 5/Tut_Selection_Sort.cpp
+++ 30 Days of Code/Day 5/Tut_Selection_Sort.cpp
@@ -9,3 +9,20 @@ void swap(int *x, int *y) // This function is used for swapping the elements of 
     *x = *y;
     *y = temp;
 }
+
+void Selection_Sort(int size, int arr[]) // Function for applying Selection Sort for a given array.
+{
+    int i, j, k;
+    for (i = 0; i < size - 1; i++)
+    {
+        for (j = k = i ; j < size ; j++)
+        {
+            if (arr[j] < arr[k])
+              {
+                   k = j;
+              }
+        }
+        swap(&arr[i], &arr[k]);
+    }
+
+} // End Selection_Sort function
