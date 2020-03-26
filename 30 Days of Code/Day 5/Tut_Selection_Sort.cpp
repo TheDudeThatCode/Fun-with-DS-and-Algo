100d60b8514986268c46d341c8beeb2ae242113b
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-27 02:26:12 +0530

    added swap function

--- 30 Days of Code/Day 5/Tut_Selection_Sort.cpp
+++ 30 Days of Code/Day 5/Tut_Selection_Sort.cpp
@@ -2,3 +2,10 @@
 
 #include <iostream>
     using namespace std;
+
+void swap(int *x, int *y) // This function is used for swapping the elements of the array.
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
