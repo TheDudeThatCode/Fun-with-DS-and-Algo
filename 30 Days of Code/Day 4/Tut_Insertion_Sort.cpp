b25a68b381eeafacfaed7fe82933b51512cbf11a
Shubhamdeep Jha <41606999+TheDudeThatCode@users.noreply.github.com>
2020-03-26 22:43:10 +0530

    Update Tut_Insertion_Sort.cpp

--- 30 Days of Code/Day 4/Tut_Insertion_Sort.cpp
+++ 30 Days of Code/Day 4/Tut_Insertion_Sort.cpp
@@ -2,3 +2,21 @@
 
 #include <iostream>
 using namespace std;
+
+
+void Insertion_Sort(int size, int arr[]) // Function for applying Insertion Sort for a given array.
+{   
+    int j, x;
+    for (int i = 1; i < size; i++)
+    {
+        j = i - 1;
+        x = arr[i];
+        while( j > -1 && arr[j] > x )
+          {
+              arr[j+1] = arr[j];
+              j--;
+          }
+        arr[j+1] = x;  
+    }
+
+} // End Insertion_Sort function
